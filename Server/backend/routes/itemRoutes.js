44bcb5f4e1b76e1a620900e7e7e0fde46e09bc32
Prianshu Sharma <prianshusharma993@gmail.com>
2024-11-13 13:26:27 +0530

    Product-Api-project

--- Server/backend/routes/itemRoutes.js
+++ Server/backend/routes/itemRoutes.js
@@ -2,5 +2,9 @@ const express = require('express');
 const router = express.Router();
 const itemController = require('../controllers/itemsController');
 router.post('/', itemController.createItem);
+router.get('/', itemController.getAllItems);
+router.get('/', itemController.getItemById);
+router.get('/', itemController.updateItem);
+router.get('/', itemController.deleteItem);
 
 module.exports = router;
