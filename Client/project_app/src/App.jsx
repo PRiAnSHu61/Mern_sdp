96a0712fd66e95a1ca15b2e1b99eb65f6f1dc6ee
Prianshu Sharma <prianshusharma993@gmail.com>
2024-11-14 12:57:12 +0530

    frontend

--- Client/project_app/src/App.jsx
+++ Client/project_app/src/App.jsx
@@ -1,14 +1,36 @@
+import React, { useState, useEffect} from 'react'
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 import './css/App.css'
-import NavBar from './components/navbar'
+import AddTrainer from './components/addTrainer'
+import GetTrainer from './components/getTrainer'
+import axios from 'axios'
 
 function App() {
-  return (
-    <>
-    <div>
-        <NavBar></NavBar>
-    </div>
-    </>
-  )
+  const{trainers, setTrainers} = useState([]);
+
+  //Fetch trainer data when the component mounts
+  useEffect(() => {
+    const fetchTrainers = async() => {
+      try{
+        const response = await axios.get('http://localhost:3000/api/trainer/');
+        setTrainers(response.data);
+      }
+      catch(error){
+        console.error('Error fetching trainers', error);
+      }
+    };
+    fetchTrainers();
+}, []);
+
+const handleAddTrainer = (newTrainer) => {
+  setTrainers([...trainers, newTrainer]);
+};
 }
+
+//const handleUpdateTrainer = (updatedTrainer) => {
+    // const updatedTrainers = trainers.map((trainer) => )
+//}
+
+
 
 export default App
